a58bb0b1c58c9394aefa6e1c2f601112dac337e0
prodigy00 <calitoalejandro184@gmail.com>
2025-05-29 10:05:39 -0400

    added types of media routes

--- src/routes/mediaRoutes.ts
+++ src/routes/mediaRoutes.ts
@@ -1,24 +1,32 @@
 import { Router } from "express";
 import multer from "multer";
-import { getAllMedia, getMediaByContent, uploadMedia } from "../controllers/mediaController";
+import { getAllMedia, uploadMedia } from "../controllers/mediaController";
 import { randomUUID } from "crypto";
 
 const router = Router();
 
 const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, "uploads/");
-	},
-	filename: (req, file, cb) => {
-		const ext = file.originalname.split('.').pop() || "bin";
+    destination: (
+        req: Express.Request,
+        file: Express.Multer.File,
+        cb: (error: Error | null, destination: string) => void
+    ) => {
+        cb(null, "uploads/");
+    },
+    filename: (
+        req: Express.Request,
+        file: Express.Multer.File,
+        cb: (error: Error | null, filename: string) => void
+    ) => {
+        const ext = file.originalname.split('.').pop() || "bin";
         const randomName = `${Date.now()}-${randomUUID()}.${ext}`;
         cb(null, randomName);
-	}
+    }
 });
 
 const upload = multer({ storage });
 const asyncHandler = (fn: any) => (req: any, res: any, next: any) =>
-	Promise.resolve(fn(req, res, next)).catch(next);
+    Promise.resolve(fn(req, res, next)).catch(next);
 
 router.post("/upload", upload.single("file"), asyncHandler(uploadMedia));
 router.get("/", asyncHandler(getAllMedia));
